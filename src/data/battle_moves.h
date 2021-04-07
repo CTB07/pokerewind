5d17dbd0c5713699518664371907e74b11c67085
luuma <luuuma@outlook.com>
2021-04-07 21:20:16 +0100

    adds the D

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -3126,7 +3126,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_R_RATE,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED | FLAG_R_RATED,
         .split = SPLIT_PHYSICAL,
     },
 
